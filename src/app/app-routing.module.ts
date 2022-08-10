905d24d0593e44fb730d450e86217f4cc3e6571d
DaniSaid <daniela.gavilan05@gmail.com>
2022-08-10 18:13:13 -0300

    carpetas reorganizadas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes} from '@angular/router';
-import { HomeComponent } from './components/home/home.component';
+import { HomeComponent } from './components/pages/home/home.component';
 import { LoginComponent } from './login/login.component';
-import { Pagina404Component } from './pagina404/pagina404.component';
-import { AboutComponent } from './about/about.component';
+import { Pagina404Component } from './components/pages/pagina404/pagina404.component';
+import { AboutComponent } from './components/pages/about/about.component';
 
 const routes : Routes = [
   {path:'', component: HomeComponent},
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,15 +9,15 @@ import { NgsRevealModule } from 'ngx-scrollreveal';
 import { DragDropModule } from '@angular/cdk/drag-drop';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import { HomeComponent } from './components/home/home.component';
-import { SectionMainComponent } from './components/section-main/section-main.component';
-import { SectionToolsComponent } from './components/section-tools/section-tools.component';
-import { SectionContactComponent } from './components/section-contact/section-contact.component';
-import { AboutComponent } from './components/about/about.component';
-import { SectionEducationComponent } from './components/section-education/section-education.component';
-import { SectionExperienceComponent } from './components/section-experience/section-experience.component';
-import { SectionProjectsComponent } from './components/section-projects/section-projects.component';
-import { Pagina404Component } from './components/pagina404/pagina404.component';
+import { HomeComponent } from './components/pages/home/home.component';
+import { SectionMainComponent } from './components/pages/home/section-main/section-main.component';
+import { SectionToolsComponent } from './components/pages/home/section-tools/section-tools.component';
+import { SectionContactComponent } from './components/pages/home/section-contact/section-contact.component';
+import { AboutComponent } from './components/pages/about/about.component';
+import { SectionEducationComponent } from './components/pages/about/section-education/section-education.component';
+import { SectionExperienceComponent } from './components/pages/about/section-experience/section-experience.component';
+import { SectionProjectsComponent } from './components/pages/home/section-projects/section-projects.component';
+import { Pagina404Component } from './components/pages/pagina404/pagina404.component';
 
 import { LoginComponent } from './login/login.component';
 import { ToolbarComponent } from './components/toolbar/toolbar.component';
