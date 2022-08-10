a61587c27c213fe427b3c98fe830455aa82b2ff9
DaniSaid <daniela.gavilan05@gmail.com>
2022-08-10 17:44:07 -0300

    reorganizando carpetas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,15 +13,15 @@ import { HomeComponent } from './components/home/home.component';
 import { SectionMainComponent } from './components/section-main/section-main.component';
 import { SectionToolsComponent } from './components/section-tools/section-tools.component';
 import { SectionContactComponent } from './components/section-contact/section-contact.component';
-import { AboutComponent } from './about/about.component';
-import { SectionEducationComponent } from './section-education/section-education.component';
-import { SectionExperienceComponent } from './section-experience/section-experience.component';
-import { SectionProjectsComponent } from './section-projects/section-projects.component';
-import { Pagina404Component } from './pagina404/pagina404.component';
+import { AboutComponent } from './components/about/about.component';
+import { SectionEducationComponent } from './components/section-education/section-education.component';
+import { SectionExperienceComponent } from './components/section-experience/section-experience.component';
+import { SectionProjectsComponent } from './components/section-projects/section-projects.component';
+import { Pagina404Component } from './components/pagina404/pagina404.component';
 
 import { LoginComponent } from './login/login.component';
-import { ToolbarComponent } from './toolbar/toolbar.component';
-import { FooterComponent } from './footer/footer.component';
+import { ToolbarComponent } from './components/toolbar/toolbar.component';
+import { FooterComponent } from './components/footer/footer.component';
 import { LoginButtonComponent} from './login-button/login-button.component';
 import { AboutButtonComponent } from './about-button/about-button.component';
 
