4c768d2fb8e81c357730a8291ec4557ab6bba715
DaniSaid <daniela.gavilan05@gmail.com>
2022-08-22 22:20:23 -0300

    funciones para crear herramienta y borrar herramienta agregadas

--- src/app/components/home/section-tools/section-tools.component.ts
+++ src/app/components/home/section-tools/section-tools.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
-import {FloatLabelType} from '@angular/material/form-field';
+import { FormBuilder, FormControl, FormGroup, NgForm} from '@angular/forms';
 import { Tool } from 'src/app/model/Tool.model';
 import { PortfolioDataService } from 'src/app/services/portfolio-data.service'
 
@@ -10,37 +9,51 @@ import { PortfolioDataService } from 'src/app/services/portfolio-data.service'
   styleUrls: ['./section-tools.component.scss']
 })
 export class SectionToolsComponent implements OnInit {
-  tools! : Tool[];
-  tool!: Tool;
+  public tools! : Tool[];
+  public tool!: Tool;
 
-  public open = false;
+  
   public editForm!: FormGroup;
+  private deleteId!: number;
+  public modalAdd: boolean = false;
+  public modalEdit: boolean = false;
+  public modalDelete: boolean = false;
 
-  floatLabelControl = new FormControl('auto' as FloatLabelType);
-
-  constructor(private portfolioData:PortfolioDataService, private formBuilder: FormBuilder) {
+  constructor(private portfolioService:PortfolioDataService) {
     
    }
 
   ngOnInit(): void {
-    this.cargarTools();
+    this.getToolList();
     this.editForm = new FormGroup({ nombre: new FormControl(''),
                                     imagen: new FormControl('')});
   }
 
-  //métodos para abrir y cerrar el formulario
+  getToolList(){
+    this.tools = [];
+    this.portfolioService.getTools().subscribe(data => {
+      this.tools = data;
+    });
+  }
+  //----------Crear---------
+  add(): void{
+    this.modalAdd = true;
+  }
+  submitAdd(add: NgForm){
+    this.portfolioService.saveTool(add.value).subscribe((toolResponse: Tool) =>{
+      console.log("herramienta agregada" + JSON.stringify(toolResponse));
+      this.getToolList();
+    });
+  }
+  closeAdd(): void{
+    this.modalAdd = false;
+  }
 
+  //----------Editar---------
   editOpen(){
-    this.open = true;
+    this.modalEdit = true;
   }
-
-  editClose(){
-    this.open = false;
-  }
-
-  //almacenar datos del formulario
-
-  submit(tool : Tool, form: FormGroup){
+  submitEdit(tool : Tool, form: FormGroup){
     console.log('id', tool.id)
     console.log('nombre', form.value.nombre);
 
@@ -49,24 +62,27 @@ export class SectionToolsComponent implements OnInit {
 
     this.tool = new Tool(tool.id, form.value.nombre, form.value.imagen);
 
-    this.portfolioData.editTool(tool.id, this.tool).subscribe(data => {
+    this.portfolioService.updateTool(tool).subscribe(data => {
       console.log("datos de herramienta editados" + JSON.stringify(data));
     });
 
-    form.value.nombre = '';
-    form.value.imagen = '';
   }
-  // métodos mat-form
-  
-  
-
-  //-------------------
-
-  cargarTools(){
-    this.tools = [];
-    this.portfolioData.getTools().subscribe(data => {
-      this.tools = data;
+  editClose(){
+    this.modalEdit = false;
+  }
+  //----------Borrar---------
+  delete(tool: Tool): void{
+    this.modalDelete = true;
+    this.deleteId = tool.id;
+  }
+  onDelete():void{
+    this.portfolioService.deleteTool(this.deleteId).subscribe(data =>{
+      console.log("herramienta borrada" + JSON.stringify(data));
+      this.getToolList();
     });
+    this.closeDelete();
   }
-
+  closeDelete(){
+    this.modalDelete = false;
+  }
 }
